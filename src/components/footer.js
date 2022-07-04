bffe092cd88a18d20cd58137401cf28a55b26d98
Damian Oportus <coportus99@gmail.com>
2022-07-04 17:14:43 -0400

    Responsiveness PERFECT

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,26 @@
 import React from "react";
 
-export default function Footer(){
+export default function Footer() {
+  const date = new Date();
+  let thisYear = date.getFullYear();
 
-    const date = new Date();
-    let thisYear = date.getFullYear();
-
-    return(
-        <footer id="footer">
-            <div>
-            <a href="https://www.linkedin.com/in/demianoportus/">
-                <i class="footer-icon fa-lg fa-brands fa-linkedin"></i>
-            </a>
-            <a href="https://github.com/DemianOportus">   
-                <i class="footer-icon fa-lg fa-brands fa-github"></i>
-            </a> 
-            <a href="/contact">
-                <i className="footer-icon fa-lg fa-solid fa-envelope"></i>
-            </a>
-                <p>© {thisYear} Copyright</p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer id="footer">
+      <div>
+        <a href="https://www.linkedin.com/in/demianoportus/" target="_blank">
+          <i class="footer-icon fa-lg fa-brands fa-linkedin"></i>
+        </a>
+        <a href="https://github.com/DemianOportus" target="_blank">
+          <i class="footer-icon fa-lg fa-brands fa-github"></i>
+        </a>
+        <a href="/contact">
+          <i
+            className="footer-icon fa-lg fa-solid fa-envelope"
+            target="_blank"
+          ></i>
+        </a>
+        <p>© {thisYear} Copyright</p>
+      </div>
+    </footer>
+  );
 }
