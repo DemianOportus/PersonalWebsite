77f40a36c41873659d3e186f5f75afcc443c8e07
Damian Oportus <coportus99@gmail.com>
2022-07-05 16:01:24 -0400

    Big update UX design Porfolio page

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,12 +3,12 @@ import Eselection from "../images/e-selection.png";
 import PersonalWebsite from "../images/personalWebsite.png";
 import DiceGame from "../images/DiceGame.png";
 
-const Entries = [
+const Projects = [
   {
     id: 1,
-    imgURL: SequenceGame,
-    projectName: "SequenceGame",
-    projectDescription:
+    img: SequenceGame,
+    title: "SequenceGame",
+    description:
       "SequenceGame is a memory game. A player has to remember a sequence of colors and be able to recite them back. Created using vanilla JS",
     sourceCode: "https://github.com/DemianOportus/SequenceGame",
     playCode: "https://demianoportus.github.io/SequenceGame/",
@@ -16,31 +16,31 @@ const Entries = [
 
   {
     id: 2,
-    imgURL: Eselection,
-    projectName: "E-Selection",
-    projectDescription:
+    img: Eselection,
+    title: "E-Selection",
+    description:
       "Website offering different services, such as landscaping, housecleaning, etc. Implemented a Database to create Users and log-in. Created using ReactJS",
     sourceCode: "https://github.com/DemianOportus/e-selection",
-    playCode: "https://github.com/DemianOportus/e-selection",
+    playCode: "https://e-selection.herokuapp.com/",
   },
   {
     id: 3,
-    imgURL: PersonalWebsite,
-    projectName: "Personal Website",
-    projectDescription:
+    img: PersonalWebsite,
+    title: "Personal Website",
+    description:
       "This is my current website. It was created using React JS, MUI Library, Router, CSS, HTML.",
     sourceCode: "https://github.com/DemianOportus/PersonalWebsite",
     playCode: "/",
   },
   {
     id: 4,
-    imgURL: DiceGame,
-    projectName: "DiceGame",
-    projectDescription:
+    img: DiceGame,
+    title: "DiceGame",
+    description:
       "Play Dice against the computer. Both numbers will be completely random, and the highest number wins. Perfect way to resolve family conflicts with a game of Dice!",
     sourceCode: "https://github.com/DemianOportus/dice",
     playCode: "https://demianoportus.github.io/SequenceGame/",
   },
 ];
 
-export default Entries;
+export default Projects;
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,21 +3,30 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import { Typography, Grid } from "@mui/material";
 
-import Entry from "../components/entry";
-import Entries from "../components/entryinfo";
+import Projects from "../components/projects";
+import Project from "../components/project";
 
 function Portfolio() {
   return (
     <>
       <Navbar />
-      <Grid containter="true" justifyContent="center" textAlign="center">
-        <Typography variant="h2">Portfolio</Typography>
-        {Entries.map(Entry)}
+
+      <Grid item>
+        <h1 className="subtitle">Portfolio</h1>
+        <hr
+          style={{
+            width: "100px",
+            backgroundColor: "#00989d",
+            height: "3px",
+            border: "none",
+            marginBottom: "60px",
+          }}
+        ></hr>
+        {Projects.map(Project)}
       </Grid>
 
       <Footer />
     </>
   );
 }
-
 export default Portfolio;
